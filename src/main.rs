3d49e90ea2233e163d1bb4984904c42ddffbc6cf
dr0th3r <dr0th3r.code@gmail.com>
2024-03-25 19:35:04 +0100

    fix: assign random ports

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind to random port"); // if we try to get port 0, the OS will assign a random port
+
+    run(listener)?.await
 }
