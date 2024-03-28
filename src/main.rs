c1dfd3802d31707c48c6c3cc9a7d15a1c6be927d
dr0th3r <dr0th3r.code@gmail.com>
2024-03-28 11:16:20 +0100

    feat: add logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::net::TcpListener;
-
-use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use std::net::TcpListener;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+    
+    let configuration = get_configuration().expect("");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
-        .expect("Failed to connect to Postgres.");
+        .expect("Failed to connect to Postgres.");           
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-
     run(listener, connection_pool)?.await
 }
