410e8c5ec5b05d9bebd09b624a079c772641e1bb
dr0th3r <dr0th3r.code@gmail.com>
2024-03-30 20:02:45 +0100

    feat: finish logging

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
 use sqlx::PgPool;
+use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     
     let configuration = get_configuration().expect("");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");           
     let address = format!("127.0.0.1:{}", configuration.application_port);
