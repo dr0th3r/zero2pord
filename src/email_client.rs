0976f6088cdb4dfd40827d424b0b20fb1056e64f
dr0th3r <dr0th3r.code@gmail.com>
2024-03-31 10:53:11 +0200

    test: improve test to check headers as well

--- src/email_client.rs
+++ src/email_client.rs
@@ -65,7 +65,7 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use secrecy::Secret;
-    use wiremock::matchers::any;
+    use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
     #[tokio::test]
@@ -78,7 +78,10 @@ mod tests {
             Secret::new(Faker.fake())
         );
 
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
