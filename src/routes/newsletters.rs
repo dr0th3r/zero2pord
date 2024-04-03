abcaa6b7a83f9bf8751564cebb617fd8587f34a9
dr0th3r <dr0th3r.code@gmail.com>
2024-04-03 21:24:32 +0200

    fix: protect against timing attacks

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -171,13 +171,18 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(
-            &credentials.username, 
-            &pool
-        )
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$aWRrMTIzNDU2Nzg5$Y0RDv+5L+UJmpQBEazl5kg".to_string()
+    );
+    if let Some((stored_user_id, stored_password_hash)) = 
+        get_stored_credentials(&credentials.username, &pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
 
     spawn_blocking_with_tracing(move || {
         verify_password_hash(
@@ -189,7 +194,9 @@ async fn validate_credentials(
     .context("Failed to spawn blocking task.")
     .map_err(PublishError::UnexpectedError)??;
 
-    Ok(user_id)
+    user_id.ok_or_else(||
+        PublishError::AuthError(anyhow::anyhow!("Unknown username."))
+    )
 }
 
 #[tracing::instrument(
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,5 +1,5 @@
 use argon2::password_hash::SaltString;
-use argon2::{Argon2, PasswordHasher};
+use argon2::{Argon2, Params, PasswordHasher};
 use sha3::Digest;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
@@ -27,7 +27,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
     pub email_server: MockServer,
     pub port: u16,
-    test_user: TestUser
+    pub test_user: TestUser
 }
 
 pub struct TestUser {
@@ -47,13 +47,14 @@ impl TestUser {
 
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
-        let password_hash = Argon2::default()
-            .hash_password(
-                self.password.as_bytes(),
-                &salt
-            )
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            argon2::Algorithm::Argon2id,
+            argon2::Version::V0x13,
+            Params::new(1500, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES($1, $2, $3)",
