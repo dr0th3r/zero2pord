ffaacd559dc2892d44177e188689f01d7353b4e1
dr0th3r <dr0th3r.code@gmail.com>
2024-04-04 19:02:02 +0200

    refactor: move auth logic into separate file

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -9,6 +9,7 @@ use actix_web::http::header::{HeaderMap, HeaderValue};
 use argon2::{Algorithm, Argon2, Params, PasswordHash, PasswordVerifier, Version};
 use uuid::Uuid;
 use crate::{domain::SubscriberEmail, email_client::{self, EmailClient}, routes::error_chain_fmt, telemetry::spawn_blocking_with_tracing};
+use crate::authentication::{AuthError, Credentials, validate_credentials};
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
@@ -99,7 +100,12 @@ pub async fn publish_newsletter(
         "username",
         &tracing::field::display(&credentials.username)
     );
-    let user_id = validate_credentials(credentials, &pool).await?;
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => PublishError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => PublishError::UnexpectedError(e.into())
+        })?;
     tracing::Span::current().record(
         "user_id", 
         &tracing::field::display(&user_id)
@@ -131,11 +137,6 @@ pub async fn publish_newsletter(
     Ok(HttpResponse::Ok().finish())
 }
 
-struct Credentials {
-    username: String,
-    password: Secret<String>
-}
-
 fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
     let header_value = headers
         .get("Authorization")
@@ -165,79 +166,3 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         password: Secret::new(password)
     })
 }   
-
-#[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
-async fn validate_credentials(
-    credentials: Credentials,
-    pool: &PgPool
-) -> Result<uuid::Uuid, PublishError> {
-    let mut user_id = None;
-    let mut expected_password_hash = Secret::new(
-        "$argon2id$v=19$m=15000,t=2,p=1$aWRrMTIzNDU2Nzg5$Y0RDv+5L+UJmpQBEazl5kg".to_string()
-    );
-    if let Some((stored_user_id, stored_password_hash)) = 
-        get_stored_credentials(&credentials.username, &pool)
-            .await
-            .map_err(PublishError::UnexpectedError)?
-    {
-        user_id = Some(stored_user_id);
-        expected_password_hash = stored_password_hash;
-    }
-
-    spawn_blocking_with_tracing(move || {
-        verify_password_hash(
-            expected_password_hash,
-            credentials.password
-        )
-    })
-    .await
-    .context("Failed to spawn blocking task.")
-    .map_err(PublishError::UnexpectedError)??;
-
-    user_id.ok_or_else(||
-        PublishError::AuthError(anyhow::anyhow!("Unknown username."))
-    )
-}
-
-#[tracing::instrument(
-    name = "Verify password hash",
-    skip(expected_password_hash, password_candidate)
-)]
-fn verify_password_hash(
-    expected_password_hash: Secret<String>,
-    password_candidate: Secret<String>
-) -> Result<(), PublishError> {
-    let expected_password_hash = PasswordHash::new(
-        expected_password_hash.expose_secret()
-    )
-    .context("Failed to parse hash in PHC string format.")
-    .map_err(PublishError::UnexpectedError)?;
-
-    Argon2::default()
-        .verify_password(
-            password_candidate.expose_secret().as_bytes(),
-            &expected_password_hash
-        )
-        .context("Invalid password.")
-        .map_err(PublishError::AuthError)
-}
-
-#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
-async fn get_stored_credentials(
-    username: &str,
-    pool: &PgPool,
-) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
-    let row: Option<_> = sqlx::query!(
-        r#"
-        SELECT user_id, password_hash
-        FROM users
-        WHERE username= $1
-        "#,
-        username,
-    )
-    .fetch_optional(pool)
-    .await
-    .context("Failed to perform a query to retrive stored credentials.")?
-    .map(|row| (row.user_id, Secret::new(row.password_hash)));
-    Ok(row)
-}
