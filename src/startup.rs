7749d9251b43d7bfbf9e74016814aa989ade9aaf
dr0th3r <dr0th3r.code@gmail.com>
2024-04-04 17:54:56 +0200

    feat: add serving home page

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe, confirm, publish_newsletter};
+use crate::routes::{health_check, subscribe, confirm, publish_newsletter, home};
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use sqlx::postgres::PgPoolOptions;
@@ -76,6 +76,7 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/", web::get().to(home))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
